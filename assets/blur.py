0c24d36c55e049645fd49cedfd292733eddc6ac7
DiR3KT <clementbahuaud@hotmail.fr>
2019-06-01 18:42:45 +0200

    a

--- assets/blur.py
+++ assets/blur.py
@@ -3,10 +3,10 @@ from PIL import ImageFilter
 from PIL import Image
 from PIL import ImageEnhance
 
-for filename in os.listdir("splash"):
-    im = Image.open("splash/"+filename)
+for filename in os.listdir("splashToBlur"):
+    im = Image.open("splashToBlur/"+filename)
     im = im.filter(ImageFilter.GaussianBlur(radius=5))
     im = ImageEnhance.Brightness(im).enhance(0.8)
     filename = str(filename)[:-4]
-    im.save("splashtest/"+filename+".jpg")
+    im.save("splash/"+filename+".jpg")
     print("done "+filename)
