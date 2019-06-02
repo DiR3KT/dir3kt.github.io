f7a98f6e3ff260684c84314a31d1aea7428d5fc8
DiR3KT <clementbahuaud@hotmail.fr>
2019-06-02 22:41:08 +0200

    Update with colored input box

--- MEDIUM/js/medium.js
+++ MEDIUM/js/medium.js
@@ -52,15 +52,21 @@ function validateGuess()
   playerGuess = document.getElementById("guess").value;
   playerGuess= playerGuess.toLowerCase();
   playerGuessRatio = compareTwoStrings(playerGuess,champions[0]);
+  trueChampName(champions[0]);
   if (playerGuessRatio>0.6)
   {
     console.log("GOOD ANSWER");
     YESURL="../assets/icons/"+champions[0]+"_0.jpg";
     document.getElementById("yesno").src=YESURL;
     document.getElementById("yesno").style.opacity=1;
-    document.getElementById("goodOne").innerHTML="Yes! It was "+ champions[0];
+    document.getElementById("goodOne").style.color="rgb(83, 184, 76)";
+    document.getElementById("goodOne").innerHTML="Yes! It was "+ trueChamp;
     good++;
     document.getElementById("goodAnswer").innerHTML=good;
+    inputBox.classList.add('good');
+    setTimeout(function(){inputBox.classList.add('blank');}, 300);
+    setTimeout(function(){inputBox.classList.remove('good');}, 300);
+    inputBox.classList.remove('blank');
   }
   else
   {
@@ -68,9 +74,14 @@ function validateGuess()
     NOURL="../assets/icons/"+champions[0]+"_0.jpg";
     document.getElementById("yesno").src=NOURL;
     document.getElementById("yesno").style.opacity=1;
-    document.getElementById("goodOne").innerHTML="No! It was "+ champions[0];
+    document.getElementById("goodOne").style.color="rgb(187, 63, 63)";
+    document.getElementById("goodOne").innerHTML="No! It was "+ trueChamp;
     wrong++;
     document.getElementById("wrongAnswer").innerHTML=wrong;
+    inputBox.classList.add('wrong');
+    setTimeout(function(){inputBox.classList.add('blank');}, 300);
+    setTimeout(function(){inputBox.classList.remove('wrong');}, 300);
+    inputBox.classList.remove('blank');
   }
   champions.shift();
   printChamp()
