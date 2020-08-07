99a5466e486028208978a1935740925a7d201453
DiR3KT <clementbahuaud@hotmail.fr>
2020-08-08 00:27:03 +0200

    Up to date for 10.16

---
+++ Laugh of Legends/js/lol.js
@@ -0,0 +1,1 @@
+document.getElementsByClassName('html')[0].style.backgroundImage = 'url(../assets/splash/' + bgimages[Math.floor(Math.random()*bgimages.length)] + ')';
--- assets/spells/python.py
+++ assets/spells/python.py
@@ -2,10 +2,30 @@ import requests
 from io import BytesIO
 from PIL import Image
 
-champions = ['aatrox', 'ahri', 'akali', 'alistar', 'amumu', 'anivia', 'annie', 'ashe', 'aurelionsol', 'azir', 'bard', 'blitzcrank', 'brand', 'braum', 'caitlyn', 'camille', 'cassiopeia', 'chogath', 'corki', 'darius', 'diana', 'draven', 'drmundo', 'ekko', 'elise', 'evelynn', 'ezreal', 'fiddlesticks', 'fiora', 'fizz', 'galio', 'gangplank', 'garen', 'gnar', 'gragas', 'graves', 'hecarim', 'heimerdinger', 'illaoi', 'irelia', 'ivern', 'janna', 'jarvaniv', 'jax', 'jayce', 'jhin', 'jinx', 'kaisa', 'kalista', 'karma', 'karthus', 'kassadin', 'katarina', 'kayle', 'kayn', 'kennen', 'khazix', 'kindred', 'kled', 'kogmaw', 'leblanc', 'leesin', 'leona', 'lissandra', 'lucian', 'lulu', 'lux', 'malphite', 'malzahar', 'maokai', 'masteryi', 'missfortune', 'mordekaiser', 'morgana', 'nami', 'nasus', 'nautilus', 'nidalee', 'nocturne', 'nunu', 'olaf', 'orianna', 'ornn', 'pantheon', 'poppy', 'pyke', 'quinn', 'rakan', 'rammus', 'reksai', 'renekton', 'rengar', 'riven', 'rumble', 'ryze', 'sejuani', 'shaco', 'shen', 'shyvana', 'singed', 'sion', 'neeko', 'sivir', 'skarner', 'sona', 'soraka', 'swain', 'sylas', 'syndra', 'tahmkench', 'taliyah', 'talon', 'taric', 'teemo', 'thresh', 'tristana', 'trundle', 'tryndamere', 'twistedfate', 'twitch', 'udyr', 'urgot', 'varus', 'vayne', 'veigar', 'velkoz', 'vi', 'viktor', 'vladimir', 'volibear', 'warwick', 'xayah', 'xerath', 'xinzhao', 'wukong', 'yasuo', 'yorick', 'zac', 'zed', 'ziggs', 'zilean', 'zoe', 'zyra']
+champions = ['monkeyking', 'yasuo', 'yuumi', 'yorick', 'zac', 'zed', 'ziggs', 'zilean', 'zoe', 'zyra', 'qiyana', 'senna', 'aphelios', 'sett', 'lillia', 'yone']
 
 for champion in champions:
-    url = "https://cdn.communitydragon.org/9.9.1/champion/"+champion+"/ability-icon/r"
+    url = "https://cdn.communitydragon.org/10.16.1/champion/"+champion+"/ability-icon/p"
+    response = requests.get(url)
+    im = Image.open(BytesIO(response.content))
+    im.convert('RGB').save(champion+"_p.jpg")
+
+    url = "https://cdn.communitydragon.org/10.16.1/champion/"+champion+"/ability-icon/q"
+    response = requests.get(url)
+    im = Image.open(BytesIO(response.content))
+    im.convert('RGB').save(champion+"_q.jpg")
+
+    url = "https://cdn.communitydragon.org/10.16.1/champion/"+champion+"/ability-icon/w"
+    response = requests.get(url)
+    im = Image.open(BytesIO(response.content))
+    im.convert('RGB').save(champion+"_w.jpg")
+
+    url = "https://cdn.communitydragon.org/10.16.1/champion/"+champion+"/ability-icon/e"
+    response = requests.get(url)
+    im = Image.open(BytesIO(response.content))
+    im.convert('RGB').save(champion+"_e.jpg")
+
+    url = "https://cdn.communitydragon.org/10.16.1/champion/"+champion+"/ability-icon/r"
     response = requests.get(url)
     im = Image.open(BytesIO(response.content))
     im.convert('RGB').save(champion+"_r.jpg")
