a1d6599f91eb343ef2ba6a1c5b0c4ddc20f29973
dir3kt <cbahuaud44@gmail.com>
2021-01-31 22:05:19 +0100

    Removed lol guesser game
    Moved to another single repository

--- Laugh of Legends/js/lol.js
+++ Laugh of Legends/js/lol.js
@@ -1,1 +1,1 @@
-document.getElementsByClassName('html')[0].style.backgroundImage = 'url(../assets/splash/' + bgimages[Math.floor(Math.random()*bgimages.length)] + ')';
+document.getElementsByClassName('html')[0].style.backgroundImage = 'url(assets/splash/' + bgimages[Math.floor(Math.random()*bgimages.length)] + ')';
